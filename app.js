f665f0ae76303d5f5ce506dd4558a1b3f2d6d301
black-code1 <legrandmahamat1@gmail.com>
2021-08-20 00:48:33 +0100

    How middleware works

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ app.use((req, res, next) => {
 
 app.use((req, res, next) => {
   console.log("In another middleware")
-});
+  res.send('<h1>Hello from express</h1>')
+})
 const server = http.createServer(app)
 
 server.listen(3000);
