1a5b5a1d6be2390cea9138a72dfad0a24367addf
black-code1 <legrandmahamat1@gmail.com>
2021-08-20 01:19:47 +0100

    Parsing Incoming Request, Limiting middleware execution

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
-const express = require("express")
+const express = require('express')
+const bodyParser = require('body-parser')
 
 const app = express()
 
-app.use('/', (req, res, next) => {
-  console.log('This always runs!')
-  next()
-})
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use('/add-product', (req, res, next) => {
-  console.log("In another middleware")
-  res.send('<h1>The add product page</h1>')
+  res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>')
+})
+
+app.post('/product', (req, res, next) => {
+  console.log(req.body)
+  res.redirect('/')
 })
 
 app.use('/', (req, res, next) => {
-  console.log("In another middleware")
-  res.send('<h1>Hello from express</h1>')
+  res.send('<h1>Hello from express!</h1>')
 })
 
 app.listen(3000);
