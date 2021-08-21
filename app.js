76239c5118158d202879055392a665f091afd084
black-code1 <legrandmahamat1@gmail.com>
2021-08-21 07:30:26 +0100

    added 404 page

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(adminRoutes)
 app.use(shopRoutes)
 
-app.use('/', (req, res, next) => {
-  res.send('<h1>Hello from express!</h1>')
+app.use((req, res, next) => {
+  res.status(404).send('<h1>Page not found</h1>')
 })
 
 app.listen(3000);
